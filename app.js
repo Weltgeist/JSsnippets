ee82490e91ac58d70d6df09c0163d0323e5c73b2
Weltgeist <narcisse118@gmail.com>
2020-04-02 23:03:47 -0400

    kelvin,celcius,farenheight newton converter feature added

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-const kelvin = 293;
+const kelvin = 293;//const 293 value
+let celcius = kelvin-273;//subsctract 273
+let fahrenheit = Math.floor(celcius*(9/5) + 32);//converted, multipled by fract.//used math library for conversion.
+console.log(`The temperature is ${fahrenheit}`);
+let newton = Math.floor(celcius*(33/100));
+console.log(`The temperature is ${newton}`);
