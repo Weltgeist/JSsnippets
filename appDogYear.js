0a696d2bf45f00ebb3daa4c2c786ee5b40ce079b
Weltgeist <narcisse118@gmail.com>
2020-04-02 23:58:47 -0400

    calculated dog age

---
+++ appDogYear.js
@@ -0,0 +1,7 @@
+let myAge = 28;//age declaration.
+let earlyYears = 2;
+earlyYears *= 10.5; // 2 first years of a dog is 1 to 10.5 years in human life.
+let laterYears = myAge - 2;
+laterYears *= 4; // later years of a dog is 1 to 4 years in human life.
+console.log(`early years: ${earlyYears} \nlater years: ${laterYears}`);
+let myAgeInDog = earlyYears + laterYears;
