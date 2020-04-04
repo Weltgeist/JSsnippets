bb3ef94564ea9c8d871d4cfa2edad1cfedcfdb2f
Weltgeist <narcisse118@gmail.com>
2020-04-04 17:26:29 -0400

    added return and error statement to getUserChoice function

--- appRockPaperScissors.js
+++ appRockPaperScissors.js
@@ -1,3 +1,8 @@
 const getUserChoice = userInput => { 
   userInput = userInput.toLowerCase();
+  if (userInput==="rock" || userInput==="paper" || userInput==="scissors"){
+    return userInput;
+  } else {
+    console.log("ERROR:Input must be either rock,paper or scissor string input.");
+  }
 };
