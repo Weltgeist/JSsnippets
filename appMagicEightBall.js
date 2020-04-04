2a65e72d3ac62cdc009c6d0adc70557d338781e1
Weltgeist <narcisse118@gmail.com>
2020-04-04 13:23:26 -0400

    Added console and 8question feature

---
+++ appMagicEightBall.js
@@ -0,0 +1,4 @@
+let userName = "Bill";
+userName? console.log(`Hello, ${userName}`) : console.log("Hello"); 
+let userQuestion = "What is the reason of human existance?";
+console.log(`${userName} asked: ${userQuestion}`);
