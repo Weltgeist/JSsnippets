3e0e8523415044f09bc59b5e6d4cbbd0acbefc94
Weltgeist <narcisse118@gmail.com>
2020-04-04 13:57:51 -0400

    added all case fore late adults, early adults, kids and exeption cases.

--- appRaceDay.js
+++ appRaceDay.js
@@ -3,5 +3,11 @@ let earlyRegistry = true;
 let runnerAge = 28;
 (runnerAge>18)&&(earlyRegistry)?raceNumber+=1000:raceNumber+=0;
 if ((runnerAge>18)&&(earlyRegistry)) {
-  console.log(`You are racenumber (${raceNumber}) and you are running at 9:30am`)
+  console.log(`You are racenumber (${raceNumber}) and you are running at 9:30am`);
+} else if ((runnerAge>18)&&(!earlyRegistry)) {
+  console.log(`You are racenumber (${raceNumber}) and you are running at 11:00am, because you late to register`);
+} else if (runnerAge<18) {
+  console.log(`You are racenumber (${raceNumber}) little one, and you are running at 12:30pm.`);
+} else {
+  console.log("You are 18?Hum!Please see registration desk.")
 }
