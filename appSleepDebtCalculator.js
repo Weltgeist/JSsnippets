b3df521b354846e40792a742a20deb25a9f5d37d
Weltgeist <narcisse118@gmail.com>
2020-04-04 20:47:32 -0400

    minor spacing corrrect

--- appSleepDebtCalculator.js
+++ appSleepDebtCalculator.js
@@ -28,9 +28,9 @@ getSleepHours = day => {
 
 getActualSleepHours = () => {
   let days = ["monday","tuesday","wednesday","thursday","friday","saturday","sunday"];
-  let sum =0;
-  for (i=0;i<days.length;i++){
-    sum+=getSleepHours(days[i]);
+  let sum = 0;
+  for (i = 0;i < days.length;i++){
+    sum += getSleepHours(days[i]);
   }
   return sum;
 };
