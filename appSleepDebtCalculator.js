8967046406ccbbd76c6eaf139ae4cee50156df8c
Weltgeist <narcisse118@gmail.com>
2020-04-04 20:30:00 -0400

    added return sleep hours per day switch case

--- appSleepDebtCalculator.js
+++ appSleepDebtCalculator.js
@@ -1,3 +1,27 @@
 getSleepHours = day => {
-
+  switch (day) {
+    case "monday":
+      return 8;
+      break;
+    case "tuesday":
+      return 6;
+      break;
+    case "wednesday":
+      return 6;
+      break;
+    case "tursday":
+      return 6;
+      break;
+    case "friday":
+      return 8;
+      break;
+    case "saturday":
+      return 10;
+      break;
+    case "sunday":
+      return 10;
+      break;
+    default:
+      break;
+  }
 }
