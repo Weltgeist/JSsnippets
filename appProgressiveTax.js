7de3f9b54f383985abf6a99e605392e5d10d3279
Weltgeist <narcisse118@gmail.com>
2020-05-29 12:35:06 -0400

    Testing some array funct

---
+++ appProgressiveTax.js
@@ -0,0 +1,6 @@
+//[2019-07-15] Challenge #379 [Easy] Progressive taxation r/dailyProgramming
+//https://www.reddit.com/r/dailyprogrammer/comments/cdieag/20190715_challenge_379_easy_progressive_taxation/
+
+const tax = () => {
+
+};
--- appSecretMessage.js
+++ appSecretMessage.js
@@ -1,1 +1,14 @@
 let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+secretMessage.pop();
+console.log(secretMessage.length);
+secretMessage.push('to','Program');
+let pos = secretMessage.indexOf('easily');
+secretMessage[pos] = 'right';
+console.log(secretMessage.join(' '));
+secretMessage.shift();
+secretMessage.unshift('Programming');
+pos = secretMessage.indexOf('get');
+secretMessage.splice(pos,5,'know,');
+console.log(secretMessage.join(' '));
+console.log(secretMessage.join(' '));
