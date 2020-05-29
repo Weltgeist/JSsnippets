ee5d2a2546f2bafb001710e2d100c11d7faf63da
Weltgeist <narcisse118@gmail.com>
2020-05-29 00:17:42 -0400

    Added other user

--- appTrainingDays.js
+++ appTrainingDays.js
@@ -41,3 +41,10 @@ const name = 'Nala' ;
 
 logEvent(name,event);
 logTime(name,days);
+
+const event2 = getRandEvent();
+const days2 = getTrainingDays(event2);
+const name2 = 'Warren';
+
+logEvent(name2, event2);
+logTime(name2, days2);
