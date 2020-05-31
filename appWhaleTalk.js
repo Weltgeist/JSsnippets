7b6d15b1e4e0969ff61478bfd682219ed3050538
Weltgeist <narcisse118@gmail.com>
2020-05-30 23:15:56 -0400

    vowel check and double e u and join uppercase

--- appWhaleTalk.js
+++ appWhaleTalk.js
@@ -6,7 +6,18 @@ for ( let i = 0; i < input.length; i++){
   //console.log(i+' '+input[i]);
   for ( let j = 0; j < vowels.length; j++){
     if (input[i] === vowels[j]){
-      
+      console.log(vowels[j]);
+      resultArray.push(input[i]);
     }
   };
+  //Whales doubles e and u
+  if (input[i] === 'e'){
+    resultArray.push('e')
+  }
+  if (input[i] === 'u'){
+    resultArray.push('u')
+  }
 };
+
+
+console.log(resultArray.join('').toUpperCase());
