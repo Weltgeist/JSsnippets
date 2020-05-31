7da61a3ebf807c080548bb083eea57d98bff7230
Weltgeist <narcisse118@gmail.com>
2020-05-30 22:55:45 -0400

    added loop

--- appWhaleTalk.js
+++ appWhaleTalk.js
@@ -3,5 +3,10 @@ let vowels = ['a','e','i','o','u'] ;
 let resultArray = [];
 
 for ( let i = 0; i < input.length; i++){
-  
+  //console.log(i+' '+input[i]);
+  for ( let j = 0; j < vowels.length; j++){
+    if (input[i] === vowels[j]){
+      
+    }
+  };
 };
