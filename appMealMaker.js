029d09ad71a592360c7316f07efe6bfade523af2
Weltgeist <narcisse118@gmail.com>
2020-06-08 23:38:24 -0400

    Added get setter methods

--- appMealMaker.js
+++ appMealMaker.js
@@ -2,6 +2,27 @@ let menu = {
   _courses: {
     appetizers: [],
     mains: [],
-    dessert: []
+    desserts: []
+  },
+  get appetizers(){
+    return this.appetizers;
+  },
+  get mains(){
+    return this.mains;
+  },
+  get desserts(){
+    return this.deserts;
+  },
+  set appetizers(appetizerChoices){
+    this.appetizers = appetizerChoices;
+  },
+  set mains(mainChoices){
+    this.appetizers = mainChoices;
+  },
+  set desserts(dessertChoices){
+    this.appetizers = dessertChoices;
+  },
+  get courses(){
+
   }
 };
