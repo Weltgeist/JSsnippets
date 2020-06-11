d0d4e992566a03ef0fed1e847fd2e1c72cc14869
Weltgeist <narcisse118@gmail.com>
2020-06-10 23:42:01 -0400

    added players and empty games

---
+++ appTeamStats.js
@@ -0,0 +1,21 @@
+const team = {
+  _players: [ 
+    {
+      firstName: 'Pablo',
+      lastname: 'Shanchez',
+      age: 28
+    }, 
+    {
+      firstName: 'Umberto',
+      lastname: 'Rodriguez',
+      age: 25 
+    },
+    {
+      firstName: 'Kroe',
+      lastname: 'Drogba',
+      age: 25
+    }
+
+  ],
+  _games: []
+};
