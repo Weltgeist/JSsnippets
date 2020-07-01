6c44448402ee391f6f4c642ccc727623d874f7fd
Weltgeist <narcisse118@gmail.com>
2020-07-01 21:21:41 +0000

    add else to cold case

--- conditional.js
+++ conditional.js
@@ -7,6 +7,8 @@ if (raining) {
 
 if (cold) {
   console.log("Make sure you pick out a scarf!");
+} else {
+  console.log("Short sleeves are fine.");
 }
 
 console.log("Now you're ready to go outside!");
